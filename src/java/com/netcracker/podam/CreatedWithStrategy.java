9e53aafa7faf38d6296b98a6c1b25a9d8da08a4d
Vitalii Titarenko <Vitalii.Tytarenko@gmail.com>
2016-12-18 08:54:56 +0200

    FileParser

--- src/java/com/netcracker/podam/CreatedWithStrategy.java
+++ src/java/com/netcracker/podam/CreatedWithStrategy.java
@@ -15,24 +15,6 @@ public class CreatedWithStrategy extends AbstractRandomDataProviderStrategy {
         super();
     }
 
-//    @Override
-//    public String getStringValue(AttributeMetadata attributeMetadata) {
-//        /**
-//         * Если поле name, то в зависимости от класса либо генерим улицу, либо
-//         * город, либо страну
-//         */
-//        if ("name".equals(attributeMetadata.getAttributeName())) {
-//            if (Street.class.equals(attributeMetadata.getPojoClass())) {
-//                return Streets.randomStreet();
-//            } else if (City.class.equals(attributeMetadata.getPojoClass())) {
-//                return Cities.randomCity();
-//            } else if (Country.class.equals(attributeMetadata.getPojoClass())) {
-//                return "Podam States of Mockitia";
-//            }
-//        }
-//        return super.getStringValue(attributeMetadata);
-//    }
-
     @Override
     public Integer getInteger(AttributeMetadata attributeMetadata) {
         if (Customer.class.equals(attributeMetadata.getPojoClass()) ||
@@ -50,28 +32,4 @@ public class CreatedWithStrategy extends AbstractRandomDataProviderStrategy {
         }
         return super.getInteger(attributeMetadata);
     }
-
-//    private enum Cities {
-//        MOSCOW, SAINT_PETERSBURG, LONDON, NEW_YORK, SHANGHAI, KARACHI, BEIJING, DELHI, PARIS, NAIROBI;
-//
-//        private static final List<Cities> values = Collections.unmodifiableList(Arrays.asList(values()));
-//        private static final int size = values.size();
-//        private static final Random random = new Random();
-//
-//        public static String randomCity() {
-//            return values.get(random.nextInt(size)).toString();
-//        }
-//    }
-//
-//    private enum Streets {
-//        RUE_ABEL, RUE_AMPERE, AVENUE_PAUL_APPELL, BOULEVARD_ARAGO, JARDINS_ARAGO, SQUARE_ARAGO, RUE_ANTOINE_ARNAULD, SQUARE_ANTOINE_ARNAULD, RUE_BERNOULLI, RUE_BEZOUT, RUE_BIOT, RUE_BORDA, SQUARE_BOREL, RUE_CHARLES_BOSSUT, RUE_DE_BROGLIE, RUE_BUFFON, AVENUE_CARNOT, BOULEVARD_CARNOT, VILLA_SADI_CARNOT, RUE_CASSINI, RUE_CAUCHY, RUE_MICHEL_CHASLES, RUE_NICOLAS_CHUQUET, RUE_CLAIRAUT, RUE_CLAPEYRON, RUE_CONDORCET, RUE_CORIOLIS, RUE_COURNOT, RUE_GASTON_DARBOUX, RUE_DELAMBRE, SQUARE_DELAMBRE, RUE_DEPARCIEUX, RUE_DE_PRONY, RUE_DESARGUES, RUE_DESCARTES, RUE_ESCLANGON, RUE_EULER;
-//
-//        private static final List<Streets> values = Collections.unmodifiableList(Arrays.asList(values()));
-//        private static final int size = values.size();
-//        private static final Random random = new Random();
-//
-//        public static String randomStreet() {
-//            return values.get(random.nextInt(size)).toString();
-//        }
-//    }
 }
